3317ce51e89ff6cd3a59dbe0cae65a0d7d1c4bf7
Michał <michal1.kowalik@orange.com>
2025-03-30 15:44:21 +0200

    Remove unused code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,21 +23,3 @@ MainWindow::~MainWindow()
     delete screenManager;
     // delete playerManager;
 }
-
-// void MainWindow::startGame(int boardSize) {
-//     boardModel = new BoardModel(boardSize);
-//     boardView = new BoardView(boardModel, this);
-
-//     connect(boardView, &BoardView::backToHomeScreen, this, &MainWindow::returnToHomeScreen);
-
-//     stackedWidget->addWidget(boardView);
-//     stackedWidget->setCurrentWidget(boardView);
-// }
-
-// void MainWindow::returnToHomeScreen() {
-//     if (boardModel && boardView) {
-//         delete boardModel;
-//         delete boardView;
-//     }
-//     stackedWidget->setCurrentWidget(homeScreen);
-// }
